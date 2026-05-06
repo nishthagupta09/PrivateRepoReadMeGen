98669c6fdc03e57f6b9838e47093ec6ffc37273c
nishthagupta09 <nishthag09@gmail.com>
2026-05-06 20:18:52 +0530

    Cors config update

--- src/main/java/com/nishtha/PrivateRepoReadMeGen/SecurityConfig.java
+++ src/main/java/com/nishtha/PrivateRepoReadMeGen/SecurityConfig.java
@@ -34,7 +34,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of("https://repo-read-me-gen.vercel.app"));
+        config.setAllowedOriginPatterns(List.of("https://repo-read-me-gen.vercel.app"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
